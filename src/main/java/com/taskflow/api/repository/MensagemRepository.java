a7330f79664959b5d38279b12c7e78c9b1ceed3d
M4rianeMS <mmariane691@gmail.com>
2026-07-05 13:10:56 -0300

    Completa funcionalidades do chat

--- src/main/java/com/taskflow/api/repository/MensagemRepository.java
+++ src/main/java/com/taskflow/api/repository/MensagemRepository.java
@@ -1,11 +1,14 @@
 package com.taskflow.api.repository;
 
 import com.taskflow.api.entity.Mensagem;
+import com.taskflow.api.entity.Projeto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface MensagemRepository extends JpaRepository<Mensagem, UUID> {
-    
+    List<Mensagem> findByProjetoOrderByDataHoraAsc(Projeto projeto);
 }
