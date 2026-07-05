4a090e361aea4f11601c2c4b31c20d828afdb8e5
fernandasfarias <fernandafariass734@gmail.com>
2026-07-05 14:31:07 -0300

    fix: adiciona metodo para listar tarefas

--- src/main/java/com/taskflow/api/controller/AtividadeController.java
+++ src/main/java/com/taskflow/api/controller/AtividadeController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/atividades")
