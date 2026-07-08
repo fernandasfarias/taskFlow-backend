bcaba69b937843dbfa6d2c675078c7944bcf7e39
fernandasfarias <fernandafariass734@gmail.com>
2026-07-08 15:36:58 -0300

    adiciona rota frontend

--- src/main/java/com/taskflow/api/config/SecurityConfig.java
+++ src/main/java/com/taskflow/api/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://taskflow-project-sw.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
